de9ca0db8acdac95946d72222f4a3ed55f6ee418
Kauane Vieira <89592039+kauanevieira@users.noreply.github.com>
2021-10-26 21:32:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function showMeSomething() {
+  alert(links.instagram)
+}
+
+const LinksSocialMedia = {
+  github: 'kauanevieira',
+  youtube: 'UCqpH3mWeI-tGNgkW-ukQROQ',
+  facebook: 'kauane.vieiraa',
+  instagram: 'kauksv',
+  twitter: 'kauksv'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
